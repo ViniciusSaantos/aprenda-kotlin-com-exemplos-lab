0e8a14486ae6415ea9fb01868d76ee181b73104f
ViniciusSaantos <154809745+ViniciusSaantos@users.noreply.github.com>
2024-01-24 22:00:57 -0300

    Atualização desafio.kt
    Botcamp Kotlin

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,45 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+// Definindo a classe ConteudoEducacional
+data class ConteudoEducacional(val nome: String, val descricao: String)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+// Definindo a interface Formacao
+interface Formacao {
+    val nome: String
+    val nivel: String
+    val conteudosEducacionais: List<ConteudoEducacional>
 
-class Usuario
+    fun matricular(aluno: Aluno)
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+// Definindo a classe Aluno
+data class Aluno(val nome: String, val matricula: String)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+// Implementando uma classe concreta que implementa a interface Formacao
+class FormacaoTecnologica(
+    override val nome: String,
+    override val nivel: String,
+    override val conteudosEducacionais: List<ConteudoEducacional>
+) : Formacao {
+    private val alunosMatriculados = mutableListOf<Aluno>()
 
-    val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    override fun matricular(aluno: Aluno) {
+        alunosMatriculados.add(aluno)
+        println("Aluno ${aluno.nome} matriculado na formação $nome - $nivel")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Exemplo de uso
+    val conteudo1 = ConteudoEducacional("Introdução a Kotlin", "Fundamentos da linguagem Kotlin")
+    val conteudo2 = ConteudoEducacional("Desenvolvimento Android", "Construção de aplicativos Android")
+
+    val formacaoAndroid = FormacaoTecnologica("Desenvolvimento Android", "Intermediário", listOf(conteudo1, conteudo2))
+
+    val aluno1 = Aluno("João", "MAT123")
+    formacaoAndroid.matricular(aluno1)
+
+    val aluno2 = Aluno("Maria", "MAT456")
+    formacaoAndroid.matricular(aluno2)
+    
+    val aluno3 = Aluno("Vinicius", "MAT620")
+    formacaoAndroid.matricular(aluno3)
 }
